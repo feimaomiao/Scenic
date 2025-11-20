9d7dd7d888357dd00960f58d6eb756af26393074
Simeon Tran <4740112+SimeonAT@users.noreply.github.com>
2025-11-20 11:45:47 -0800

    Good progress but crashes XPlane

--- src/scenic/simulators/xplane/location.py
+++ src/scenic/simulators/xplane/location.py
@@ -1,20 +1,35 @@
-try:
-  from xpc import XPlaneConnect
-except ImportError as exception:
-  raise RuntimeError('The X-Plane interface requires XPlaneConnect.') from exception
+from xpc import XPlaneConnect
 
-def printPosition(client):
-  fields = ["Lat", "Lon", "Alt", "Pitch", "Roll", "Yaw", "Gear"]
+LAT = 0
+LON = 1
+ALT = 2
+PITCH = 3
+ROLL = 4
+YAW = 5
+GEAR = 6
+FIELDS = [i for i in range(LAT, GEAR + 1)]
 
-  position = client.getPOSI()
+def printPosition(position):
+  assert(len(position) == len(FIELDS))
+
   for i in range(0, len(position)):
-    print(f"{fields[i]}: {position[i]}")
+    print(f"{FIELDS[i]}: {position[i]}")
   
   print()
+  return
+
+def setPosition(client, field, newValue):
+  position = list(client.getPOSI())
+  position[field] = newValue
+  client.sendPOSI(position)
   return
 
 if __name__ == "__main__":
   client = XPlaneConnect()
 
   print(f"Position at the center of the runway:")
-  printPosition(client)
+  position = client.getPOSI()
+  printPosition(position)
+
+  setPosition(client, LAT, position[LAT] + 100)
+  printPosition(position)
