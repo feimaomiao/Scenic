78f6e9b72e134a4d821f4a590a84dfd0e6b40720
Simeon Tran <4740112+SimeonAT@users.noreply.github.com>
2025-11-21 12:50:37 -0800

    Good progress

--- src/scenic/simulators/xplane/location.py
+++ src/scenic/simulators/xplane/location.py
@@ -5,39 +5,41 @@ from time import sleep
 
 from xpc import XPlaneConnect
 
-X_DREF = "sim/flightmodel/position/local_x"
-Y_DREF = "sim/flightmodel/position/local_y"
-Z_DREF = "sim/flightmodel/position/local_z"
-COORDS_DREFS = [X_DREF, Y_DREF, Z_DREF]
-
 SLEEP_TIME_SECONDS = 2
 
 class XPlaneWrapper():
+  X_DREF = "sim/flightmodel/position/local_x"
+  Y_DREF = "sim/flightmodel/position/local_y"
+  Z_DREF = "sim/flightmodel/position/local_z"
+  COORDS_DREFS = [X_DREF, Y_DREF, Z_DREF]
 
   def __init__(self):
     self.client = XPlaneConnect()
     return
 
   def getPosition(self):
-    position = self.client.getDREFs(COORDS_DREFS)
-    return (position[0], position[1], position[2])
+    position = self.client.getDREFs(self.COORDS_DREFS)
+    return tuple([int(coord[0]) for coord in position])
 
   def setOffset(self, dref, offset):
     self.client.sendDREF(dref, self.client.getDREF(dref)[0] + offset)
     return
 
   def setLocation(self, location):
-    self.client.sendDREFs(COORDS_DREFS, [location[0], location[1], location[2]])
+    self.client.sendDREFs(self.COORDS_DREFS, [location[0], location[1], location[2]])
     return
 
 
 if __name__ == "__main__":
   client = XPlaneWrapper()
 
+  CENTER = client.getPosition()
   locations = [
-    (851.549560546875, 350.7986755371094, -31377.404296875),  # top left corner
+    (851.549560546875, 350.7986755371094, -31377.404296875),   # top left corner
     (808.9561157226562, 351.1387634277344, -31355.494140625),  # top right corner
-    client.getPosition()
+    (-225.74005126953125, 337.68798828125, -33897.8203125)     # bottom left corner
+    (-266.25006103515625, 337.9598083496094, -33882.296875)    # bottom right corner
+    CENTER
   ]
 
   for location in locations:
