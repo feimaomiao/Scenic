9007494c3ce270e46999180b85d19f7302ebaf9b
Cheuk Pui Lam <lcpmatthew@gmail.com>
2025-12-05 23:05:37 -0800

    feat: Random flight failures and correct termination

--- src/scenic/simulators/xplane/common.py
+++ src/scenic/simulators/xplane/common.py
@@ -51,10 +51,12 @@ class XPlaneWrapper():
 
   def setAutopilotMode(self, mode):
     self.client.sendDREF("sim/operation/prefs/ai_flies_aircraft", mode)
+    return
+
+  def sendCTRL(self, command):
+    self.client.sendCTRL(command)
+    return
 
   def getCrashed(self):
-    crashedInt = int(self.client.getDREF("sim/flightmodel2/misc/has_crashed")[0])
-    crashed = crashedInt != 0
-
-    print(f"Plane Crashed: {crashed}")
+    crashed = bool(int(self.client.getDREF("sim/flightmodel2/misc/has_crashed")[0]))
     return crashed
